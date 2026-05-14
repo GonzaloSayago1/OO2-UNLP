823b19fbed80e6725a56be8cbb8256b0ff15026e
GonzaloSayago1 <gonzalosayago03@gmail.com>
2026-05-14 11:54:54 -0300

    agrego ejercicios

---
+++ Patrones/ejercicioPrestamos/ejercicio26/src/main/java/ar/edu/info/unlp/ejercicioPrestamos/Prenda.java
@@ -0,0 +1,16 @@
+package ar.edu.info.unlp.ejercicioPrestamos;
+
+public abstract class Prenda {
+	private double liquidez;
+
+	public Prenda(double liquidez) {
+		this.liquidez = liquidez;
+	}
+	
+	public double calcularValor()
+	{
+		return this.calcularExtra() * this.liquidez;
+	}
+	
+	protected abstract double calcularExtra();
+}
